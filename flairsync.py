31479fca0dce4e074d74a2c7956c0c6b5f8d541e
Logan Hanks <logan@reddit.com>
2011-07-18 12:15:52 -0700

    Tool to sync flair on site with a local csv file.

---
+++ flairsync.py
@@ -0,0 +1,121 @@
+#!/usr/bin/python
+
+import argparse
+import csv
+import htmlentitydefs
+import re
+import sys
+
+import redditclient
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        add_help=False,
+        description='sync subreddit flair with with a csv file')
+
+    parser.add_argument('subreddit', metavar='SUBREDDIT')
+    parser.add_argument('csvfile', metavar='CSVFILE')
+
+    parser.add_argument('--help', action='help', default=argparse.SUPPRESS,
+                        help='show this help message and exit')
+    parser.add_argument('-b', '--batch_size', type=int, default=100,
+                        help='number of users to read at a time from the site')
+    parser.add_argument('-c', '--cookie_file',
+                        help='if given, save session cookie in this file')
+    parser.add_argument('-h', '--host', default='http://reddit.com',
+                        help='URL of reddit API server')
+    return parser.parse_args()
+
+def ynprompt(prompt):
+    return raw_input(prompt).lower().startswith('y')
+
+def log_in(host, cookie_file):
+    client = redditclient.RedditClient(host, cookie_file)
+    while not client.log_in():
+        print 'login failed'
+    return client
+
+def flair_from_csv(path):
+    f = csv.reader(file(path))
+    # skip header row
+    f.next()
+    return dict((r[0], (r[1], r[2])) for r in f)
+
+def flair_from_reddit(client, subreddit, batch_size):
+    def u(html):
+        if html is None:
+            return None
+        def decode_entity(m):
+            text = m.group(0)
+            if text.startswith('&#'):
+                try:
+                    if text.startswith('&#x'):
+                        return unichr(int(text[3:-1]), 16)
+                    else:
+                        return unichr(int(text[2:-1]))
+                except ValueError:
+                    pass  # ignore
+            else:
+                try:
+                    return unichr(htmlentitydefs.name2codepoint[text[1:-1]])
+                except KeyError:
+                    pass  # ignore
+            # fallthrough; on error just return original text
+            return text
+        return re.sub(r'&#?\w+;', decode_entity, html)
+
+    return dict((u(r[0]), (u(r[1]), u(r[2])))
+                for r in client.flair_list(subreddit, batch_size=batch_size))
+
+def diff_flair(left, right):
+    """returns: (modifications, additions, deletions)"""
+    left_users = frozenset(left)
+    right_users = frozenset(right)
+    common_users = left_users & right_users
+    added_users = right_users - left_users
+    removed_users = left_users - right_users
+    modifications = []
+    for u in common_users:
+        if left[u] != right[u]:
+            modifications.append((u, right[u][0], right[u][1]))
+    additions = [(u, right[u][0], right[u][1]) for u in added_users]
+    return modifications, additions, removed_users
+
+def main():
+    config = parse_args()
+
+    print 'Parsing csv file: %s ...' % config.csvfile
+    csv_flair = flair_from_csv(config.csvfile)
+
+    print 'Connecting to %s ...' % config.host
+    client = log_in(config.host, config.cookie_file)
+
+    print 'Fetching current flair from site ...'
+    reddit_flair = flair_from_reddit(client, config.subreddit,
+                                     config.batch_size)
+
+    print 'Computing differences ...'
+    modifications, additions, deletions = diff_flair(reddit_flair, csv_flair)
+
+    print '\nmodifications:'
+    print '\n'.join(['  %s -> %r %s' % mod for mod in modifications])
+    print '\nadditions:'
+    print '\n'.join(['  %s -> %r %s' % mod for mod in additions])
+    print '\ndeletions:'
+    print '\n'.join(['  %s' % u for u in deletions])
+
+    if ((modifications or additions or deletions)
+        and ynprompt('\napply the above changes? [y/N] ')):
+
+        for user, text, css in modifications + additions:
+            print 'setting flair for user %s ...' % user
+            client.flair(config.subreddit, user, text, css)
+
+        for user in deletions:
+            print 'deleting flair for user %s ...' % user
+            client.unflair(config.subreddit, user)
+
+        print 'Done!'
+
+if __name__ == '__main__':
+    main()
---
+++ redditclient.py
@@ -0,0 +1,87 @@
+import cookielib
+import getpass
+import json
+import urllib
+import urllib2
+
+class RedditClient:
+    def __init__(self, host='http://reddit.com', cookie_file=None):
+        while host.endswith('/'):
+            host = host[:-1]
+        self.host = host
+
+        if cookie_file:
+            self.cookies = cookielib.LWPCookieJar(cookie_file)
+            try:
+                self.cookies.load(ignore_discard=True)
+            except IOError:
+                pass  # ignore if file doesn't exist yet
+        else:
+            self.cookies = cookielib.CookieJar()
+
+    def _url(self, path, sr=None):
+        if not path.startswith('/'):
+            path = '/' + path
+        if sr:
+            prefix = '%s/r/%s/' % (self.host, sr)
+        else:
+            prefix = self.host
+        return '%s%s.json' % (prefix, path)
+
+    def _post(self, url, **data):
+        return self._request('POST', url, **data)
+
+    def _get(self, url, **data):
+        return self._request('GET', url, **data)
+
+    def _request(self, method, url, **data):
+        data = urllib.urlencode(data)
+        if method == 'GET':
+            if '?' in url:
+                url = '%s&%s' % (url, data)
+            else:
+                url = '%s?%s' % (url, data)
+            data = None
+        req = urllib2.Request(url, data)
+        self.cookies.add_cookie_header(req)
+        resp = urllib2.urlopen(req)
+        self.cookies.extract_cookies(resp, req)
+        return json.load(resp)
+
+    def log_in(self):
+        for cookie in self.cookies:
+            if cookie.name == 'reddit_session':
+                return True
+        user = raw_input('reddit username: ')
+        password = getpass.getpass('reddit password: ')
+        response = self._post(self._url('/api/login'),
+                              user=user, passwd=password)
+        for cookie in self.cookies:
+            if cookie.name == 'reddit_session':
+                try:
+                    self.cookies.save(ignore_discard=True)
+                except NotImplementedError:
+                    pass  # ignore
+                return True
+        return False
+
+    def flair_list(self, subreddit, batch_size=100):
+        after = None
+        while True:
+            kw = dict(limit=batch_size)
+            if after:
+                kw['after'] = after
+            result = self._get(self._url('/api/flairlist', sr=subreddit), **kw)
+            after = result.get('next')
+            for user in result.get('users', []):
+                yield (user.get('user'), user.get('flair_text'),
+                       user.get('flair_css_class'))
+            if not after:
+                break
+
+    def flair(self, subreddit, user, text, css_class):
+        self._post(self._url('/api/flair', sr=subreddit),
+                   name=user, text=text, css_class=css_class)
+
+    def unflair(self, subreddit, user):
+        self._post(self._url('/api/unflair', sr=subreddit), name=user)
